c2a7572d2a690a331184a4aa97b1b4ce6b16a27c
justynmeza <justinmesa2013@gmail.com>
2023-05-18 00:04:25 -0500

    add generated token for users

--- models/users.py
+++ models/users.py
@@ -1,5 +1,6 @@
 from config import *
 from flask import jsonify, request
+import datetime, secrets
 
 class User():
 
@@ -48,13 +49,15 @@ class User():
             payload = []
             content = {}
             for result in rv:
+                id_game = result[0]
                 content = {
-                    'id':result[0],
+                    'id':id_game,
                     'name':result[1],
                     'lastname':result[2],
                     'email':result[3],
                     'username':result[4],
-                    'password':result[5]
+                    'password':result[5],
+                    'token':self.create_token(id_game)
                 }
                 payload.append(content)
                 content = {}
@@ -62,3 +65,65 @@ class User():
             return jsonify(payload)
         except (Exception, psycopg2.DatabaseError) as error:
             return jsonify({'information':error})
+        
+
+    def create_token(self, id):
+        try:
+            if (self.consult_token(id)):
+                self.delete_token(id)
+            token = self.generate_token()
+            
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO "tblToken" ("IdUser", "Token", "ExpiredTime") '+
+                           f"VALUES ({id}, '{token}', '{self.consult_time()}')")
+            cursor.connection.commit()
+            cursor.close()
+            
+            return token
+        except (Exception, psycopg2.DatabaseError) as error:
+            return error
+        
+    def consult_token(self, id):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(f'SELECT EXISTS (SELECT * FROM "tblToken" WHERE "IdUser" = {id})')
+            rv = cursor.fetchall()
+            cursor.close()
+            payload = rv[0][0]
+            print(payload)
+            return payload
+        except (Exception, psycopg2.DatabaseError) as error:
+            return error
+        
+    def delete_token(self, id):
+        try:
+            cursor = connection.cursor()
+            cursor.execute(f'DELETE FROM "tblToken" WHERE "IdUser" = {id}')
+            cursor.connection.commit()
+            cursor.close()
+            print('ok')
+            return 'ok'
+        except (Exception, psycopg2.DatabaseError) as error:
+            return error
+        
+    def consult_time(self):
+        ahora = datetime.datetime.now()
+        hora = ahora.hour
+        minuto = ahora.minute + 10
+        segundo = ahora.second
+        if minuto >= 60:
+            hora += 1
+            minuto -= 60
+            if hora > 24:
+                hora = 1
+        return f"{hora}:{self.minute_config(minuto)}:{segundo}"
+    
+    def minute_config(self, minute):
+        if len(str(minute)) < 2:
+            return f'0{minute}'
+        else:
+            return minute
+    
+    def generate_token(self):
+        token = secrets.token_hex(32)
+        return token
